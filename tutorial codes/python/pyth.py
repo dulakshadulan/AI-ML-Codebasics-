02597c65230939e53ae0b7d12faca3cbd4719dab
Dulaksha Dulan <dulakshadulan@gmail.com>
2025-05-17 21:42:01 +0530

    Renamed

--- tutorial codes/python/pyth.py
+++
@@ -1,7 +0,0 @@
-def calculate_area(base, height):
-    print("__name__", __name__)
-    return 1/2*base*height
-
-if __name__ == "__main__":
-    a = calculate_area(10,20)
-    print("area", a)
--- tutorial codes/python/thon.py
+++
@@ -1,5 +0,0 @@
-import pyth
-print("I an in caller.py")
-
-
-pyth.calculate_area(4,8)
